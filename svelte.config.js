5773dfa7518fc37c5181dc85c215ee0edf83a732
Jacob Merrell <jacob@oldgrowth.city>
2022-07-05 19:14:15 -0400

    Initial setup

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 			assets: 'docs'
 		}),
 		paths: {
-			base: dev ? '' : '/your-repo-name',
+			base: dev ? '' : '/sveltekit-ssg-test',
 		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
